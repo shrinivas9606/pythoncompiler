d898a3fb1c84ec47b61511544735019ac973775a
Shri Reddy <narhareshrinivas170@gmail.com>
2023-01-28 22:03:30 +0530

    initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn OnlineCompiler.wsgi --log-file -
---
+++ pycompiler/urls.py
@@ -0,0 +1,11 @@
+
+from django.urls import path, include
+
+# import views
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name="indexpage"),
+    path('runcode', views.runcode, name="runcode"),
+]
